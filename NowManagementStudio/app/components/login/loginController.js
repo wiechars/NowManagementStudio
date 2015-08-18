333cd79fd3a3217f03b481ac4373d14c52f73d06
wiechars <rwiecha@isystemsnow.com>
2015-08-18 14:30:29 -0400

    Reorganized xenon projet to better conform with angular standards.

---
+++ NowManagementStudio/app/components/login/loginController.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+app.controller('loginController', ['$scope', '$location', 'authService', function ($scope, $location, authService) {
+    $scope.userName = "";
+    $scope.loginData = {
+        userName: "",
+        password: ""
+    };
+
+    $scope.message = "";
+
+    $scope.login = function () {
+     
+        authService.login($scope.loginData).then(function (response) {
+            $location.path('/app/contacts');
+            $scope.userName = "Wiecha";
+
+        },
+         function (err) {
+             $scope.message = err.error_description;
+         });
+    };
+
+
+    $scope.logout = function () {
+        authService.logOut();
+        $location.path('/login');
+
+    };
+
+}]);
