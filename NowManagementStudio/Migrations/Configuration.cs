3df8f4e8a0651aac8d0945f6b2bf6227746bd97b
wiechars <rwiecha@isystemsnow.com>
2015-08-27 12:27:07 -0400

    Updated Configuration code first seed.

--- NowManagementStudio/Migrations/Configuration.cs
+++ NowManagementStudio/Migrations/Configuration.cs
@@ -1,22 +1,59 @@
 namespace NowManagementStudio.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<NowManagementStudio.Models.AuthContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
-
-            // register mysql code generator
-            SetSqlGenerator("MySql.Data.MySqlClient", new MySql.Data.Entity.MySqlMigrationSqlGenerator());
-
-            SetHistoryContextFactory("MySql.Data.MySqlClient", (conn, schema) => new MySqlHistoryContext(conn, schema));
         }
 
         protected override void Seed(NowManagementStudio.Models.AuthContext context)
         {
+            if (!context.Roles.Any(r => r.Name == "SuperAdmin"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole { Name = "SuperAdmin" };
 
+                manager.Create(role);
+            }
+
+            if (!context.Roles.Any(r => r.Name == "AppAdmin"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole { Name = "AppAdmin" };
+
+                manager.Create(role);
+            }
+
+            if (!context.Users.Any(u => u.UserName == "admin"))
+            {
+                var store = new UserStore<IdentityUser>(context);
+                var manager = new UserManager<IdentityUser>(store);
+                var user = new IdentityUser { UserName = "admin" };
+
+                manager.Create(user, ".W3b3r420");
+                manager.AddToRole(user.Id, "AppAdmin");
+            }
+
+            if (!context.Users.Any(u => u.UserName == "admin"))
+            {
+                var store = new UserStore<IdentityUser>(context);
+                var manager = new UserManager<IdentityUser>(store);
+                var user = new IdentityUser { UserName = "admin" };
+
+                manager.Create(user, ".W3b3r420");
+                manager.AddToRole(user.Id, "SuperAdmin");
+                manager.AddToRole(user.Id, "AppAdmin");
+            }
         }
     }
 }
